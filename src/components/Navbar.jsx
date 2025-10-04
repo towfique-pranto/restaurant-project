49ccbec7479f4a73ec3efc8a3522d29ad9791190
Towfique Kabir <towfique.pranto@gmail.com>
2025-10-04 21:02:38 +0600

    navigation bar setup added side button and down arrow on dishes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,13 @@ import {Link} from 'react-scroll'
 import { IoRestaurantOutline } from "react-icons/io5";
 import Button from '../layout/Button';
 import {AiOutlineClose, AiOutlineMenuUnfold} from "react-icons/ai";
+import {BiChevronDown} from "react-icons/bi";
 
 
 const Navbar = () => {
     const [menu, setMenu] = useState(false);
     const handleChange = () => { setMenu(!menu); };
-    const closeMenu = () => {setMenu(false);};
+    //const closeMenu = () => {setMenu(false);};
 
     return (
         <div className="fixed w-full">
@@ -25,6 +26,7 @@ const Navbar = () => {
                         <div className="relative group">
                             <div className="flex items-center gap-1">
                                 <Link to="dishes" spy={true} smooth={true} duration={500} className='hover:text-orange-500 transition-all cursor-pointer'>Dishes</Link>
+                                <BiChevronDown className="cursor-pointer" size={25}/>
                             </div>
                             <ul className="absolute hidden space-y-2 group-hover:block bg-white border border-gray-300 shadow-md rounded-lg p-5">
                                 <li>
@@ -48,13 +50,23 @@ const Navbar = () => {
 
                         <Button title="Login"/>
                     </nav>
-                    <div>
+                    <div className="md:hidden flex items-center">
                         {menu ?
                                 (<AiOutlineClose size={25} onClick={handleChange}/>):(
                                 <AiOutlineMenuUnfold size={25} onClick={handleChange}/>
                             )
                         }
                     </div>
+                </div>
+                <div className={`${menu ? "translate-x-0": "-translate-x-full"} lg:hidden flex flex-col absolute bg-black text-white left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}>
+                    <Link to="home" spy={true} smooth={true} duration={500} className='hover:text-orange-500 transition-all cursor-pointer'>Home</Link>
+                    <Link to="dishes" spy={true} smooth={true} duration={500} className='hover:text-orange-500 transition-all cursor-pointer'>Dishes</Link>
+                    <Link to="about" spy={true} smooth={true} duration={500} className='hover:text-orange-500 transition-all cursor-pointer'>About</Link>
+                <Link to="menu" spy={true} smooth={true} duration={500} className='hover:text-orange-500 transition-all cursor-pointer'>Menu</Link>
+                <Link to="review" spy={true} smooth={true} duration={500} className='hover:text-orange-500 transition-all cursor-pointer'>Reviews</Link>
+
+                <Button title="Login" />
+
                 </div>
             </div>
         </div>
