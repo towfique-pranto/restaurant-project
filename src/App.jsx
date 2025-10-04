e25211f7cd9561b59f2d2764daf2653668bf909c
Towfique Kabir <towfique.pranto@gmail.com>
2025-10-04 21:23:25 +0600

    created home.jsx file and added order button on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
 
 const App = () => {
     return (
         <div>
             <Navbar />
+
+            <main>
+                <div id='home'>
+                    <Home/>
+                </div>
+            </main>
         </div>
     )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from "../layout/Button.jsx";
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <h1>Crafted with Passion, Served with Love.</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid asperiores aut dolorem
+                    fugiat magnam modi rem saepe soluta voluptatum!
+                </p>
+                <div>
+                    <Button title="Order Now!"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
