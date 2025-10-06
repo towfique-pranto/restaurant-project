1fe66e97378c10113628aabd09d69c852803bc6b
Towfique Kabir <towfique.pranto@gmail.com>
2025-10-06 21:59:39 +0600

    made 6 cards with pictures

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import DishCard from "../layout/DishCard.jsx";
-import img1 from "../assets/img/img1.jpg"
+import img1 from "../assets/img/img1.png"
+import img2 from "../assets/img/img2.png"
+import img3 from "../assets/img/img3.png"
+import img4 from "../assets/img/img4.png"
+import img5 from "../assets/img/img5.png"
+import img6 from "../assets/img/img6.png"
 
 const Dishes = () => {
     return (
-        <div>
-            <h1>Our Dishes</h1>
+        <div className="min-h-screen flex flex-col justify-center items-center lg:px-32 px-5">
+            <h1 className="text-4xl font-semibold text-center pt-24 pb-10">Our Dishes</h1>
 
-            <div>
-                <DishCard img={img1} title="Desi Crusine" price="BDT 500"/>
+            <div className="flex flex-wrap gap-8 justify-center">
+                <DishCard img={img1} title="Desi Crusine" price="500"/>
+                <DishCard img={img2} title="Desi Crusine" price="500"/>
+                <DishCard img={img3} title="Desi Crusine" price="500"/>
+                <DishCard img={img4} title="Desi Crusine" price="500"/>
+                <DishCard img={img5} title="Desi Crusine" price="500"/>
+                <DishCard img={img6} title="Desi Crusine" price="500"/>
             </div>
-        </div>
-    )
+        </div>   )
 }
 export default Dishes
--- src/layout/DishCard.jsx
+++ src/layout/DishCard.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 import {BsStarFill, BsStarHalf} from "react-icons/bs";
 import Button from "./Button.jsx";
 
+
 const DishCard = (props) => {
     return (
-        <div className="w-1/4 lg:w-1/4 p-5 rounded-lg">
-            <img src={props.img} alt="img" />
-            <div>
-                <h3>
+
+        <div className="w-60 lg:w-1/4 p-5 shadow-[0_3px_8px_rgba(0,0,0,0.24)] rounded-lg">
+            <img className="rounded-xl" src={props.img} alt="img"/>
+            <div className="space-y-4">
+                <h3 className="font-semibold text-center text-xl pt-6">
                     {props.title}
                 </h3>
-                <div>
-                    <BsStarFill/>
-                    <BsStarFill/>
-                    <BsStarFill/>
-                    <BsStarFill/>
-                    <BsStarHalf/>
+                <div className="flex flex-row justify-center">
+                    <BsStarFill className="text-orange-500"/>
+                    <BsStarFill className="text-orange-500"/>
+                    <BsStarFill className="text-orange-500"/>
+                    <BsStarFill className="text-orange-500"/>
+                    <BsStarHalf className="text-orange-500"/>
                 </div>
-                <div>
-                    <h3>{props.price}</h3>
+                <div className="flex flex-row items-center justify-center gap-4">
+                    <h3 className="font-semibold text-sm">৳{props.price}</h3>
                     <Button title="Buy Now!"/>
                 </div>
             </div>
