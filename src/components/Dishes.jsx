2a8c4620c181b1c6cbdd67de92f5cc52646e99bb
Towfique Kabir <towfique.pranto@gmail.com>
2025-10-15 19:38:02 +0600

    Fixed stuff in dishes also removes more pictures

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -6,6 +6,9 @@ import img3 from "../assets/img/img3.png"
 import img4 from "../assets/img/img4.png"
 import img5 from "../assets/img/img5.png"
 import img6 from "../assets/img/img6.png"
+import img7 from "../assets/img/menu1.jpg"
+import img8 from "../assets/img/menu2.jpg"
+import img9 from "../assets/img/menu3.webp"
 
 const Dishes = () => {
     return (
@@ -16,9 +19,12 @@ const Dishes = () => {
                 <DishCard img={img1} title="Desi Crusine" price="500"/>
                 <DishCard img={img2} title="Desi Crusine" price="500"/>
                 <DishCard img={img3} title="Desi Crusine" price="500"/>
-                <DishCard img={img4} title="Desi Crusine" price="500"/>
-                <DishCard img={img5} title="Desi Crusine" price="500"/>
-                <DishCard img={img6} title="Desi Crusine" price="500"/>
+                <DishCard img={img4} title="Vegetarian" price="500"/>
+                <DishCard img={img5} title="Vegetarian" price="500"/>
+                <DishCard img={img6} title="Vegetarian" price="500"/>
+                <DishCard img={img7} title="Fast Food" price="500"/>
+                <DishCard img={img8} title="Fast Food" price="500"/>
+                <DishCard img={img9} title="Fast Food" price="500"/>
             </div>
         </div>   )
 }
